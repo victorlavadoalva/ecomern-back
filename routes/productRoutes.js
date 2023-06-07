572764fc08c229e9ef05bcaf452afc8ffa212c48
victor <victor.lavado15@hotmail.com>
2023-06-06 23:36:07 -0500

    modificado product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -63,7 +63,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.get(":/id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const product = await Product.findById(id);
@@ -74,4 +74,19 @@ router.get(":/id", async (req, res) => {
   }
 });
 
+router.get("/category/:category", async (req, res) => {
+  const { category } = req.params;
+  try {
+    let products;
+    const sort = { _id: -1 };
+    if (category == "all") {
+      products = await Product.find().sort(sort);
+    } else {
+      products = await Product.find({ category }).sort(sort);
+    }
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 module.exports = router;
